3de384d3737425d34c3a9a439bffeed9f70ca369
zuoyinchen <yinchen.zuo@wenba100.com>
2020-09-04 13:31:13 +0800

    ts

---
+++ demo.ts
@@ -0,0 +1,15 @@
+function zz() {
+    let a: string = '12312312'
+    console.log(a)
+    const obj: {
+        name: string
+    } = {
+        name: 'cccc'
+    }
+    let arr: string [] = ['111', `2222`]
+    class person{}
+    let p: person = new person()
+    const xx: () => string = () => {return 'ssss'}
+    // 基础静态类型&& 对象静态类型 （对象，数组，类类型 函数类型）
+}
+zz()
